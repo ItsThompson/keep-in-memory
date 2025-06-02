13a625ccdceee18732ae68f372ad57229bd6714d
thompson <hi.thompson@hotmail.com>
2025-06-02 12:53:22 +0100

    feat(timer): added button to skip timer (`done` button)

--- frontend/src/components/ui/timer.tsx
+++ frontend/src/components/ui/timer.tsx
@@ -10,12 +10,12 @@ export default function Timer({
     onTimeout,
 }: TimerProps) {
     const [timeInSeconds, setTimeInSeconds] = useState(durationInSeconds);
-    const timerRef = useRef<NodeJS.Timeout | null>(null); // Store timer reference
+    const timerRef = useRef<ReturnType<typeof setInterval> | null>(null);
 
     useEffect(() => {
         timerRef.current = setInterval(() => {
             setTimeInSeconds((seconds) => {
-                if (seconds <= 0) {
+                if (seconds <= 1) {
                     clearInterval(timerRef.current!);
                     return 0;
                 }
@@ -24,9 +24,11 @@ export default function Timer({
         }, 1000);
 
         return () => {
-            clearInterval(timerRef.current!); // Cleanup on unmount
+            if (timerRef.current) {
+                clearInterval(timerRef.current);
+            }
         };
-    }, []);
+    }, [durationInSeconds]);
 
     useEffect(() => {
         if (timeInSeconds === 0 && onTimeout) {
@@ -34,16 +36,38 @@ export default function Timer({
         }
     }, [timeInSeconds, onTimeout]);
 
+    // Skip button handler
+
+    const handleSkip = () => {
+        if (timerRef.current) {
+            clearInterval(timerRef.current);
+        }
+
+        setTimeInSeconds(0);
+
+        if (onTimeout) {
+            onTimeout();
+        }
+    };
+
     return (
-        <div className="bg-primary text-secondary font-bold p-3 m-2 rounded">
-            {durationInSeconds <= 60 ? (
-                <p>{`${timeInSeconds}`}</p>
-            ) : (
-                <p>
-                    {`${Math.floor(timeInSeconds / 60)}`.padStart(2, "0")}:
-                    {`${timeInSeconds % 60}`.padStart(2, "0")}
-                </p>
-            )}
+        <div className="flex">
+            <div className="bg-primary text-secondary font-bold p-3 m-2 rounded">
+                {durationInSeconds <= 60 ? (
+                    <p>{timeInSeconds}</p>
+                ) : (
+                    <p>
+                        {`${Math.floor(timeInSeconds / 60)}`.padStart(2, "0")}:
+                        {`${timeInSeconds % 60}`.padStart(2, "0")}
+                    </p>
+                )}
+            </div>
+            <button
+                className="bg-secondary text-primary font-bold p-3 m-2 rounded"
+                onClick={handleSkip}
+            >
+                done
+            </button>
         </div>
     );
 }
